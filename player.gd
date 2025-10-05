a436472ef71d19e512a7debd07edb52823e981e3
manu2w2 <victor2rescod@gmail.com>
2025-10-04 18:32:18 -0600

    camara and base map

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # Velocidad del personaje (puedes cambiarla para que camine más rápido o más lento)
-@export var speed: float = 120.0
+@export var speed: float = 200.0
 
 # Conectamos el nodo AnimatedSprite2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
